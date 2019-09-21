65cfefab2eb76da8eda8e446fd7b6b6f4b3b7d26
KillerInk <KillerInk@email.de>
2019-09-21 10:37:23 +0200

    fix RawStackPipe2

--- app/src/main/java/freed/cam/apis/camera2/modules/RawStackPipe2.java
+++ app/src/main/java/freed/cam/apis/camera2/modules/RawStackPipe2.java
@@ -9,10 +9,12 @@ import com.troop.freedcam.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.MappedByteBuffer;
 
 import freed.cam.apis.basecamera.CameraWrapperInterface;
 import freed.cam.apis.basecamera.modules.ModuleHandlerAbstract;
 import freed.cam.apis.basecamera.parameters.AbstractParameter;
+import freed.cam.apis.camera2.modules.helper.ImageCaptureHolder;
 import freed.cam.apis.camera2.parameters.ae.AeManagerCamera2;
 import freed.cam.ui.themesample.handler.UserMessageHandler;
 import freed.dng.DngProfile;
@@ -74,91 +76,129 @@ public class RawStackPipe2 extends PictureModuleApi2 {
         return tmpFolder +"burst"+BurstCounter.getImageCaptured();
     }
 
+
+    @Override
+    public void DoWork() {
+        imageStored=0;
+        super.DoWork();
+    }
+
     /**
      * Reset the capturesession to preview
      *
      */
-
-
+    private int imageStored;
     @Override
     public void internalFireOnWorkDone(File file)
     {
+        mBackgroundHandler.post(() ->{
         Log.d(TAG, "internalFireOnWorkDone"  + " burstCount/imagecount:" + BurstCounter.getBurstCount() + "/" +BurstCounter.getImageCaptured());
+        imageStored++;
         if (workFinishEventsListner != null)
             workFinishEventsListner.internalFireOnWorkDone(file);
         else {
-            Log.d(TAG, "internalFireOnWorkDone BurstCount:" + BurstCounter.getBurstCount() + " imageCount:" + BurstCounter.getImageCaptured());
+            Log.d(TAG, "internalFireOnWorkDone BurstCount:" + BurstCounter.getBurstCount() + " imageCount:" + BurstCounter.getImageCaptured() + " images stored: " + imageStored);
             if (BurstCounter.getBurstCount() >= BurstCounter.getImageCaptured()) {
                 filesSaved.add(file);
                 Log.d(TAG, "internalFireOnWorkDone Burst addFile");
             }
-            if (BurstCounter.getBurstCount() == BurstCounter.getImageCaptured()) {
-                Log.d(TAG, "internalFireOnWorkDone Burst done");
-
-                new Thread(() -> {
-                    try {
-                        byte[] input = RawToDng.readFile(filesSaved.get(0));
-                        if (input != null)
-                            Log.d(TAG, "input size: " + input.length);
-                        RawStack rawStack = new RawStack();
-                        int upshift;
-                        if (SettingsManager.get(SettingKeys.forceRawToDng).get())
-                        {
-                            //if input data is 12bit only scale it by 2, else it would clip high/lights = 14bit
-                            if (SettingsManager.get(SettingKeys.support12bitRaw).get())
-                                upshift = 2;
-                            else //shift 10bit input up to 14bit
-                                upshift = 4;
-                        }
-                        else //use stock dngcreator, dont scale it up till we found a way to manipulate black and whitelvl from the capture result
-                            upshift = 0;
-
-                        DngProfile dngProfile;
-                        if (SettingsManager.get(SettingKeys.useCustomMatrixOnCamera2).get() && SettingsManager.getInstance().getDngProfilesMap().get(input.length) != null) {
-                            dngProfile = SettingsManager.getInstance().getDngProfilesMap().get(input.length);
-                            if (upshift > 0)
-                            {
-                                dngProfile.setBlackLevel(dngProfile.getBlacklvl() << upshift);
-                                dngProfile.setWhiteLevel(dngProfile.getWhitelvl() << upshift);
-                            }
-                        }
-                        else
-                            dngProfile = currentCaptureHolder.getDngProfile(upshift);
-
-
-                        if (SettingsManager.get(SettingKeys.MATRIX_SET).get() == null ||SettingsManager.get(SettingKeys.MATRIX_SET).get().equals(SettingsManager.getInstance().getResString(R.string.off_)))
-                            dngProfile.matrixes = currentCaptureHolder.getMatrix();
-                        else
-                            dngProfile.matrixes = SettingsManager.getInstance().getMatrixesMap().get(SettingsManager.get(SettingKeys.MATRIX_SET).get());
-
-                        if(dngProfile.matrixes == null)
-                            dngProfile.matrixes = currentCaptureHolder.getMatrix();
-
-                        rawStack.setShift(upshift);
-                        rawStack.setFirstFrame(input, dngProfile.getWidth(), dngProfile.getHeight());
-
-                        for (int i = 1; i < filesSaved.size(); i++) {
-                            byte[] nextinput = RawToDng.readFile(filesSaved.get(i));
-                            rawStack.stackNextFrame(nextinput);
-                            UserMessageHandler.sendMSG("Stacked:" + i, false);
-                        }
-
-
-                        ExifInfo exifInfo = currentCaptureHolder.getExifInfo();
-                        String fileout = cameraUiWrapper.getActivityInterface().getStorageHandler().getNewFilePath(SettingsManager.getInstance().GetWriteExternal(), "") + ".dng";
-                        rawStack.saveDng(dngProfile, dngProfile.matrixes, fileout, exifInfo);
-                        fireOnWorkFinish(new File(fileout));
-                        for (File filetodel : filesSaved) {
-                            filetodel.delete();
-                        }
-                        filesSaved.clear();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-
-
+            if (BurstCounter.getBurstCount() == imageStored) {
+                Log.d(TAG, "start stack");
+                startStack();
             }
+
+        }
+        });
+    }
+
+    @Override
+    protected void finishCapture() {
+        super.finishCapture();
+
+    }
+
+    @Override
+    public void onRdyToSaveImg(ImageCaptureHolder holder) {
+        super.onRdyToSaveImg(holder);
+    }
+
+    private static volatile boolean stacking = false;
+    private void startStack()
+    {
+        if (BurstCounter.getBurstCount() == BurstCounter.getImageCaptured()) {
+            Log.d(TAG, "internalFireOnWorkDone Burst done");
+
+            if (stacking)
+                return;
+
+            new Thread(() -> {
+                stacking = true;
+                try {
+                    byte[] input = RawToDng.readFile(filesSaved.get(0));
+                    //MappedByteBuffer input = RawToDng.readFileToMemoryMap(filesSaved.get(0));
+                    if (input != null)
+                        Log.d(TAG, "input size: " + input.length);
+                    RawStack rawStack = new RawStack();
+                    int upshift;
+                    if (SettingsManager.get(SettingKeys.forceRawToDng).get())
+                    {
+                        //if input data is 12bit only scale it by 2, else it would clip high/lights = 14bit
+                        if (SettingsManager.get(SettingKeys.support12bitRaw).get())
+                            upshift = 2;
+                        else //shift 10bit input up to 14bit
+                            upshift = 4;
+                    }
+                    else //use stock dngcreator, dont scale it up till we found a way to manipulate black and whitelvl from the capture result
+                        upshift = 0;
+
+                    DngProfile dngProfile;
+                    if (SettingsManager.get(SettingKeys.useCustomMatrixOnCamera2).get() && SettingsManager.getInstance().getDngProfilesMap().get(input.length) != null) {
+                        dngProfile = SettingsManager.getInstance().getDngProfilesMap().get(input.length);
+                        if (upshift > 0)
+                        {
+                            dngProfile.setBlackLevel(dngProfile.getBlacklvl() << upshift);
+                            dngProfile.setWhiteLevel(dngProfile.getWhitelvl() << upshift);
+                        }
+                    }
+                    else
+                        dngProfile = currentCaptureHolder.getDngProfile(upshift);
+
+
+                    if (SettingsManager.get(SettingKeys.MATRIX_SET).get() == null ||SettingsManager.get(SettingKeys.MATRIX_SET).get().equals(SettingsManager.getInstance().getResString(R.string.off_)))
+                        dngProfile.matrixes = currentCaptureHolder.getMatrix();
+                    else
+                        dngProfile.matrixes = SettingsManager.getInstance().getMatrixesMap().get(SettingsManager.get(SettingKeys.MATRIX_SET).get());
+
+                    if(dngProfile.matrixes == null)
+                        dngProfile.matrixes = currentCaptureHolder.getMatrix();
+
+                    rawStack.setShift(upshift);
+                    rawStack.setFirstFrame(input, dngProfile.getWidth(), dngProfile.getHeight());
+
+                    for (int i = 1; i < filesSaved.size(); i++) {
+                        byte[] nextinput = RawToDng.readFile(filesSaved.get(i));
+                        rawStack.stackNextFrame(nextinput);
+                        Log.d(TAG,"Stacked:" + i);
+                        //nextinput.clear();
+                        UserMessageHandler.sendMSG("Stacked:" + i, false);
+                    }
+
+
+                    ExifInfo exifInfo = currentCaptureHolder.getExifInfo();
+                    String fileout = cameraUiWrapper.getActivityInterface().getStorageHandler().getNewFilePath(SettingsManager.getInstance().GetWriteExternal(), "") + ".dng";
+                    rawStack.saveDng(dngProfile, dngProfile.matrixes, fileout, exifInfo);
+                    fireOnWorkFinish(new File(fileout));
+                    for (File filetodel : filesSaved) {
+                        filetodel.delete();
+                    }
+                    filesSaved.clear();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                stacking = false;
+            }).start();
+
+
         }
     }
 }
